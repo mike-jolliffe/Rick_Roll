561394fe86023a63d4f53a3e4c9bd4c4f33e661f
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-15 13:52:53 -0700

    modified message to Mike

--- tiny_web_app.py
+++ tiny_web_app.py
@@ -6,17 +6,14 @@ app = Flask(__name__)
 def hello():
     response = '''<?xml version="1.0" encoding="UTF-8"?>
                     <Response>
-                        <Say>Hello, Mike! You have been rick rolled.</Say>
+                        <Say>Hello, big guy! You have just been rick rolled.</Say>
                         <Play>https://afternoon-gorge-22076.herokuapp.com/rick_roll.mp3</Play>
                     </Response>'''
     return Response(response, mimetype='text/xml')
 
 @app.route('/rick_roll.mp3')
 def song():
-    # response = make_response(render_template('audio.html'))
-    #
-    # #response.mimetype = 'audio/mpeg'
-    # return response
+    
     return app.send_static_file('rick_roll.mp3')
 
 
